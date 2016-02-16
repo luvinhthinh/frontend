7834a19b5c59327dbd1107c34248ccb42f367a9f
luvinhthinh <luvinhthinh.mono@gmail.com>
2016-02-16 15:13:51 +0800

    Add services to Sales Order

--- src/main/java/com/dev/backend/service/SalesOrderService.java
+++ src/main/java/com/dev/backend/service/SalesOrderService.java
@@ -2,6 +2,7 @@ package com.dev.backend.service;
 
 import java.util.List;
 
+import com.dev.backend.domain.OrderLine;
 import com.dev.backend.domain.SalesOrder;
 
 public interface SalesOrderService {
@@ -11,4 +12,7 @@ public interface SalesOrderService {
 	public List<SalesOrder> selectAll();
 	public SalesOrder findSalesOrderById(String id);
 	public boolean isSalesOrderExist(SalesOrder salesOrder);
+	
+	public boolean validate(SalesOrder salesOrder);
+	public List<OrderLine> consolidateOrderLine(List<OrderLine> lines);
 }
